9a44f49e2098e6e35fe53698a2bf3589b857ae0b
linkoid <36754150+linkoid@users.noreply.github.com>
2024-07-06 11:14:23 -0400

    Reorganized project into separate assemblies

--- SmiteLib.Core/Logging/IUsesLogger.cs
+++ SmiteLib.Core/Logging/IUsesLogger.cs
@@ -1,22 +1,7 @@
-﻿using System.Collections;
-
-namespace SmiteLib.Logging;
+﻿namespace SmiteLib.Logging;
 
 public interface IUsesLogger
 {
     public ILogger Logger { get; set; }
 }
 
-internal static class UsesLoggerExtensions
-{
-    public static void ForceChildrenUseOwnLogger(this IUsesLogger parent, IEnumerable children)
-    {
-        foreach (var child in children)
-        {
-            if (child is not IUsesLogger loggerUser)
-                continue;
-
-            loggerUser.Logger = parent.Logger;
-		}
-    }
-}
---
+++ SmiteLib.Injection/System.Runtime.CompilerServices/InternalImplementationOnlyAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project below for more information.
+// https://github.com/dotnet/roslyn/tree/main?tab=MIT-1-ov-file
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// This is a marker attribute that can be put on an interface to denote that only internal implementations
+    /// of that interface should exist.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+    internal sealed class InternalImplementationOnlyAttribute : Attribute
+    {
+    }
+}
