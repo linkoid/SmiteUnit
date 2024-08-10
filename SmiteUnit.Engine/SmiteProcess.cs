2310024fad0c78fc61322554964d36ca0a0a6dff
linkoid <36754150+linkoid@users.noreply.github.com>
2024-08-10 13:03:00 -0400

    Update SmiteProcess.cs

--- SmiteUnit.Engine/SmiteProcess.cs
+++ SmiteUnit.Engine/SmiteProcess.cs
@@ -1,116 +1,115 @@
 ﻿using System.Diagnostics;
 using System.Collections.Generic;
-using SmiteUnit.Internal;
+using SmiteUnit.Engine.Internal;
 
-namespace SmiteUnit.Engine
+namespace SmiteUnit.Engine;
+
+public class SmiteProcess : System.IDisposable
 {
-	public class SmiteProcess : System.IDisposable
+	public ArgumentList Arguments { get; }
+
+	public bool UseSubprocess 
+	{ 
+		get => !_process.StartInfo.UseShellExecute;
+		set
+		{
+			_process.StartInfo.UseShellExecute = !value;
+			_process.StartInfo.RedirectStandardInput = value;
+			_process.StartInfo.RedirectStandardOutput = value;
+			_process.StartInfo.RedirectStandardError = value;
+		}
+	}
+
+	public bool HasExited => _process.HasExited;
+	public int ExitCode => _process.ExitCode;
+
+	public string WorkingDirectory { get => _process.StartInfo.WorkingDirectory; set => _process.StartInfo.WorkingDirectory = value; }
+
+	public int RunTimeout { get; set; } = -1;
+
+	[System.Obsolete]
+	public Process InternalProcess => _process;
+
+
+	internal readonly Process _process = new();
+
+	public readonly RedirectionStreamReader Output;
+	public readonly RedirectionStreamReader Error;
+
+	public SmiteProcess(string filePath, string arguments = "")
 	{
-		public ArgumentList Arguments { get; }
+		_process.StartInfo = new ProcessStartInfo(filePath);
+		Arguments = new(arguments);
 
-		public bool UseSubprocess 
-		{ 
-			get => !_process.StartInfo.UseShellExecute;
-			set
-			{
-				_process.StartInfo.UseShellExecute = !value;
-				_process.StartInfo.RedirectStandardInput = value;
-				_process.StartInfo.RedirectStandardOutput = value;
-				_process.StartInfo.RedirectStandardError = value;
-			}
-		}
+		WorkingDirectory = System.IO.Path.GetDirectoryName(filePath) ?? "";
+		UseSubprocess = true;
 
-		public bool HasExited => _process.HasExited;
-		public int ExitCode => _process.ExitCode;
+		Output = new(_process, StandardStream.Output);
+		Error = new(_process, StandardStream.Error);
+	}
 
-		public string WorkingDirectory { get => _process.StartInfo.WorkingDirectory; set => _process.StartInfo.WorkingDirectory = value; }
+	public bool Run()
+	{
+		_process.StartInfo.Arguments = Arguments.ToString();
+		return RunInternal();
+	}
 
-		public int RunTimeout { get; set; } = -1;
+	public bool RunTest(ISmiteId testId)
+	{
+		_process.StartInfo.Arguments = $"{Arguments} --SmiteUnit.test:{testId}";
+		return RunInternal();
+	}
 
-		[System.Obsolete]
-		public Process InternalProcess => _process;
+	private bool RunInternal()
+	{
+		if (!_process.Start())
+			return false;
 
+		Output.StartListening();
+		Error.StartListening();
 
-		internal readonly Process _process = new();
+		bool exited = _process.WaitForExit(RunTimeout);
 
-		public readonly RedirectionStreamReader Output;
-		public readonly RedirectionStreamReader Error;
+		Output.StopListening();
+		Error.StopListening();
 
-		public SmiteProcess(string filePath, string arguments = "")
+		return exited;
+	}
+
+	private bool _isDisposed;
+	protected virtual void Dispose(bool disposing)
+	{
+		if (_isDisposed) return;
+
+		if (!_process.HasExited)
 		{
-			_process.StartInfo = new ProcessStartInfo(filePath);
-			Arguments = new(arguments);
-
-			WorkingDirectory = System.IO.Path.GetDirectoryName(filePath) ?? "";
-			UseSubprocess = true;
-
-			Output = new(_process, StandardStream.Output);
-			Error = new(_process, StandardStream.Error);
-		}
-
-		public bool Run()
-		{
-			_process.StartInfo.Arguments = Arguments.ToString();
-			return RunInternal();
-		}
-
-		public bool RunTest(ISmiteId testId)
-		{
-			_process.StartInfo.Arguments = $"{Arguments} --SmiteUnit.test:{testId}";
-			return RunInternal();
-		}
-
-		private bool RunInternal()
-		{
-			if (!_process.Start())
-				return false;
-
-			Output.StartListening();
-			Error.StartListening();
-
-			bool exited = _process.WaitForExit(RunTimeout);
-
-			Output.StopListening();
-			Error.StopListening();
-
-			return exited;
-		}
-
-		private bool _isDisposed;
-		protected virtual void Dispose(bool disposing)
-		{
-			if (_isDisposed) return;
-
-			if (!_process.HasExited)
-			{
 #if NET5_0_OR_GREATER || NETCOREAPP3_0_OR_GREATER
-				_process.Kill(true);
+			_process.Kill(true);
 #else
-				_process.Kill();
+			_process.Kill();
 #endif
-			}
-
-			if (disposing)
-			{
-				// Dispose managed state (managed objects)
-				Output.Dispose();
-				Error.Dispose();
-				_process.Close();
-				_process.Dispose();
-			}
-
-			_isDisposed = true;
 		}
 
-		public void Dispose()
+		if (disposing)
 		{
-			Dispose(disposing: true);
-			System.GC.SuppressFinalize(this);
+			// Dispose managed state (managed objects)
+			Output.Dispose();
+			Error.Dispose();
+			_process.Close();
+			_process.Dispose();
 		}
 
-		~SmiteProcess()
-		{
-			Dispose(disposing: false);
-		}
+		_isDisposed = true;
+	}
+
+	public void Dispose()
+	{
+		Dispose(disposing: true);
+		System.GC.SuppressFinalize(this);
+	}
+
+	~SmiteProcess()
+	{
+		Dispose(disposing: false);
 	}
 }
